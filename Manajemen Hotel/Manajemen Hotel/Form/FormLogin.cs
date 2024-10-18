32d5deb41d1282f61061d3303e7c28a18fe35d95
nafis123131 <ramadhanath1@outlook.com>
2024-10-18 21:39:08 +0700

    Add files via upload

---
+++ Manajemen Hotel/Manajemen Hotel/Form/FormLogin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_Hotel
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxMinimize_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxMinimize, "Minimize");
+        }
+
+        private void pictureBoxClose_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxClose, "Close");
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBoxMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBoxShow_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxShow, "Show Password");
+        }
+
+        private void pictureBoxHidden_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBoxHidden, "Hide Password");
+        }
+
+        private void pictureBoxShow_Click(object sender, EventArgs e)
+        {
+            pictureBoxShow.Hide();
+            txtPassword.UseSystemPasswordChar = false;
+            pictureBoxHidden.Show();
+        }
+
+        private void pictureBoxHidden_Click(object sender, EventArgs e)
+        {
+            pictureBoxHidden.Hide();
+            txtPassword.UseSystemPasswordChar = true;
+            pictureBoxShow.Show();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
